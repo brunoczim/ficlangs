251f3535ae68ef9db917678a1c50780b34821c02
brunoczim <brunoczim@gmail.com>
2020-05-04 15:43:48 -0300

    added vowel trapezoid

--- site/postcss.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = ({ file, options, env }) => ({
-  parser: false,
-  plugins: {
-    'postcss-import': { root: file.dirname },
-    'postcss-preset-env': options['postcss-preset-env'] || false,
-    'cssnano': env === 'production' ? options.cssnano : false
-  }
-})
--- site/webpack.config.js
+++ site/webpack.config.js
@@ -1,50 +1,55 @@
-const path = require("path")
-const HtmlWebpackPlugin = require("html-webpack-plugin")
-const outputDir = path.join(__dirname, "dist/")
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
+const outputDir = path.join(__dirname, 'dist/')
 
-const isProd = process.env.NODE_ENV === "production"
+const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
   entry: {
-    "index": "./src/Index.bs.js",
-    "star/index": "./src/Star_Index.bs.js",
-    "star/phonology": "./src/Star_Phonology.bs.js",
-    "star/texts/index": "./src/Star_Texts_Index.bs.js",
-    "star/texts/creation-of-the-world":
-      "./src/Star_Texts_CreationOfTheWorld.bs.js"
+    'index': './src/Index.bs.js',
+    'star/index': './src/Star_Index.bs.js',
+    'star/phonology': './src/Star_Phonology.bs.js',
+    'star/texts/index': './src/Star_Texts_Index.bs.js',
+    'star/texts/creation-of-the-world':
+      './src/Star_Texts_CreationOfTheWorld.bs.js'
   },
-  mode: isProd ? "production" : "development",
-  devtool: "source-map",
+  mode: isProd ? 'production' : 'development',
+  devtool: 'source-map',
   output: {
     path: outputDir,
-    filename: "[name].js"
+    filename: '[name].js'
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./src/index.html",
-      filename: "index.html",
+      template: './src/index.html',
+      filename: 'index.html',
       inject: false
     }),
     new HtmlWebpackPlugin({
-      template: "./src/star/index.html",
-      filename: "star/index.html",
+      template: './src/star/index.html',
+      filename: 'star/index.html',
       inject: false
     }),
     new HtmlWebpackPlugin({
-      template: "./src/star/phonology.html",
-      filename: "star/phonology.html",
+      template: './src/star/phonology.html',
+      filename: 'star/phonology.html',
       inject: false
     }),
     new HtmlWebpackPlugin({
-      template: "./src/star/texts/index.html",
-      filename: "star/texts/index.html",
+      template: './src/star/texts/index.html',
+      filename: 'star/texts/index.html',
       inject: false
     }),
     new HtmlWebpackPlugin({
-      template: "./src/star/texts/creation-of-the-world.html",
-      filename: "star/texts/creation-of-the-world.html",
+      template: './src/star/texts/creation-of-the-world.html',
+      filename: 'star/texts/creation-of-the-world.html',
       inject: false
-    })
+    }),
+    new CopyPlugin([
+      'img',
+      { from: 'src/img', to: 'img' },
+    ]),
   ],
   devServer: {
     compress: true,
@@ -63,8 +68,8 @@ module.exports = {
       },
       {
         test: /\.ttf$/,
-        use: ["url-loader"]
-      }
+        use: ['url-loader']
+      },
     ]
   }
 }
